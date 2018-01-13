6d988d1a278d652575d466a64c974eae160a416f
SDS 800 <jeremyhyman@gmail.com>
2018-01-13 05:24:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   artistName: "song titles"
 };
 
-function updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi") {
+function updatePlaylist('Phil Ochs', "Here's to the State of Mississippi") {
   return playlist
   
 }
